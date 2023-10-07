8a068accbf3dc93e5c1ed5393353f7e83ee3b241
Stylianos Gregoriou <s.gregoriou@cyi.ac.cy>
2023-10-07 14:56:23 +0300

    Cleaned dead code

--- Critical_mass_analysis/custom_library/Chebyshev_polynomials.py
+++ Critical_mass_analysis/custom_library/Chebyshev_polynomials.py
@@ -1,145 +1,172 @@
+'''
+Scalar and matrix functions for constructing Chebyshev polynomial terms of the first kind and an approximate matrix sign .
+TODO: Check if functions work for complex arguments.
+'''
+
 import numpy as np
 from numpy.linalg import matrix_power
 from math import factorial
 from scipy.linalg import fractional_matrix_power
 from scipy import linalg
 
-# import custom_library.auxiliary
 
-# __all__ = []
+# SCALAR FUNCTIONS
 
-def scalar_Chebyshev_polynomials_function(x, n):
-  '''TODO: Write description'''
+# SCALAR TRIGONOMETRIC FUNCTIONS
 
-  Tn_minus1 = 1
+def scalar_approximate_cosine_function(x, n=10):
+  '''Implementation of a truncated Taylor series for approximating the cosine function.'''
+
+  assert isinstance(x, float), 'Input argument must be a real number.'
+  assert (n >= 0) and isinstance(n, int), 'n must be a positive integer.'
+
+  # Change the overall sign if the input value is in a range [ (4*k+1)*π/2, (4*k+3)*π/2 ), k integer
+  sign = +1
+  if ((x//(np.pi/2))%4 in [1, 2]):
+    sign = -1
+
+  # Map the input value to the [0, π/2) interval
+  x = x%np.pi
+  if (x//(np.pi/2) == 1):
+    x = np.pi - x
+
+  sum = 0.0
+  for k in range(n):
+    sum += ((-1)**(k)/factorial(2*k))*x**(2*k)
+
+  return sign*sum
+
+
+def scalar_approximate_sine_function(x, n=10):
+  '''Implementation of a truncated Taylor series for approximating the sine function.'''
+
+  assert isinstance(x, float), 'Input argument must be a real number.'
+  assert (n >= 0) and isinstance(n, int), 'n must be a positive integer.'
+
+  # Change the overall sign if the input value is in a range [ (2*k+1)*π, (k+1)*2π ), k integer
+  sign = +1
+  if ((x//np.pi)%2 == 1):
+    sign = -1
+
+  # Map the input value to the [0, π/2) interval
+  x = x%np.pi
+  if (x//(np.pi/2) == 1):
+    x = np.pi - x
+
+  sum = 0.0
+  for k in range(n):
+    sum += ((-1)**(k)/factorial(2*k+1))*x**(2*k+1)
+
+  return sign*sum
+
+
+# SCALAR CHEBYSHEV POLYNOMIAL FUNCTIONS
+
+def scalar_Chebyshev_polynomial_term_function(x, n):
+  '''Iterative implementation of the recursive definition of the Chebyshev polynomials of the first kind for scalar argument.'''
+
+  assert isinstance(x, float), 'Input argument must be a real number.'
+  assert isinstance(n, int) and (n >= 0), 'The order parameter n must be a non-negative integer.'
+  
   if (n == 0):
-    return Tn_minus1
-  else:
-    Tn = x
-    for order in range(1, n):
-      temp = Tn
-      Tn = 2*x*Tn - Tn_minus1
-      Tn_minus1 = temp
+     return 1
+  
+  Tn_minus1, Tn = 1, x
+  for _ in range(1, n):
+    Tn_minus1, Tn = Tn, 2*x*Tn - Tn_minus1
 
-    return Tn
+  return Tn
 
-def _matrix_sign_function_array_function(array):
-  '''TODO: Write description'''
 
-  array = np.array(array)
-  array_norm_squared = np.matmul(array, np.conjugate(array.T))
-  inverse_array_norm = fractional_matrix_power(array_norm_squared, -0.5)
-  sign_function_array = np.matmul(array, inverse_array_norm)
+def scalar_Chebyshev_polynomial_term_recursive_function(x, n):
+  '''Recursive implementation of the recursive definition of the Chebyshev polynomials of the first kind for scalar argument.
+  Note: for n>=40 it calculates very slowly.'''
 
-  return sign_function_array
+  assert isinstance(x, float), 'The argument of the polynomials must be a real number.'
+  assert isinstance(n, int) and (n >= 0), 'The order parameter n must be a non-negative integer.'
 
-def _recursive_matrix_Chebyshev_polynomials_function(X, n):
+  cache_dictionary = {0: 1, 1: x}
 
-	if (n == 0):
-		return np.identity(np.shape(X)[0], dtype=np.complex_)
-	elif (n == 1):
-		return X
-	else:
-		return 2.0*np.matmul(X, _recursive_matrix_Chebyshev_polynomials_function(X, n-1)) - _recursive_matrix_Chebyshev_polynomials_function(X, n-2)
+  if n in cache_dictionary:
+    return cache_dictionary[n]
+  
+  cache_dictionary[n] = 2*x*scalar_Chebyshev_polynomial_term_recursive_function(x, n-1) - scalar_Chebyshev_polynomial_term_recursive_function(x, n-2)
 
-def matrix_Chebyshev_polynomials_function(X, n):
+  return cache_dictionary[n]
 
-  Tn_minus1 = np.identity(np.shape(X)[0], dtype=np.complex_)
+
+# MATRIX FUNCTIONS
+
+# MATRIX TRIGONOMETRIC FUNCTIONS
+
+def matrix_approximate_cosine_function(X, n=10):
+  '''Implementation of a truncated Taylor series for approximating the matrix cosine function.'''
+
+  X = np.array(X)
+  assert (X.ndim == 2) and (X.shape[0] == X.shape[1]), 'Input matrix must be a 2-dimensional square array.'
+  assert (n >= 0) and isinstance(n, int), 'n must be a positive integer greater than 1.'
+
+  # Use the square of the matrix for the summation to facilitate the calculation
+  squared_matrix = matrix_power(X, 2)
+  sum = np.zeros_like(X)
+  for k in range(n):
+    sum += ((-1)**(k)/factorial(2*k))*matrix_power(squared_matrix, k)
+
+  return sum
+
+
+def matrix_approximate_cosine_function(X, n=10):
+  '''Implementation of a truncated Taylor series for approximating the matrix cosine function.'''
+
+  X = np.array(X)
+  assert (X.ndim == 2) and (X.shape[0] == X.shape[1]), 'Input matrix must be a 2-dimensional square array.'
+  assert (n >= 0) and isinstance(n, int), 'n must be a positive integer greater than 1.'
+
+  # Use the square of the matrix for the summation to facilitate the calculation
+  sum = np.zeros_like(X)
+  for k in range(n):
+    sum += ((-1)**(k)/factorial(2*k+1))*matrix_power(X, 2*k+1)
+
+  return sum
+
+
+# MATRIX CHEBYSHEV POLYNOMIAL FUNCTIONS
+
+def matrix_Chebyshev_polynomial_term_function(X, n):
+  '''Iterative implementation of the recursive definition of the Chebyshev polynomials of the first kind for matrix argument.'''
+
+  X = np.array(X)
+  assert (X.ndim == 2) and (X.shape[0] == X.shape[1]), 'Input matrix must be a 2-dimensional square array.'
+  assert isinstance(n, int) and (n >= 0), 'The order parameter n must be a non-negative integer.'
 
   if (n == 0):
-    return Tn_minus1
-  else:
-    Tn = X
-    for order in range(1, n):
-      temp = Tn
-      Tn = 2*np.matmul(X, Tn) - Tn_minus1
-      Tn_minus1 = temp
+    return np.identity(np.shape(X)[0])
 
-    return Tn
+  Tn_minus1, Tn = np.identity(np.shape(X)[0]), X
+  for _ in range(1, n):
+    Tn_minus1, Tn = Tn, 2*np.matmul(X, Tn) - Tn_minus1
+  
+  return Tn
 
-def _scalar_approximate_cosine_function(x, n=10):
 
-    assert (n >= 0) and isinstance(n, int), 'n must be a positive integer greater than 1.'
+def matrix_Chebyshev_polynomial_term_recursive_function(X, n):
+  '''Recursive implementation of the recursive definition of the Chebyshev polynomials of the first kind for matrix argument.
+  Note: for n>=40 it calculates very slowly.'''
 
-    sign = +1
-    x = x%(2.*np.pi)
-    if (x > np.pi/2.):
-      if (x < np.pi):
-        x = np.pi - x
-        sign = -1
-      elif (x < 3.*np.pi/2.):
-        x = x - np.pi
-        sign = -1
-      else:
-        x = 2*np.pi - x
-        sign = +1
-          
-    sum = 1.
-    for k in range(1, n):
-        sum += ((-1)**(k)/factorial(2*k))*x**(2*k)
+  X = np.array(X)
+  assert (X.ndim == 2) and (X.shape[0] == X.shape[1]), 'Input matrix must be a 2-dimensional square array.'
+  assert isinstance(n, int) and (n >= 0), 'The order parameter n must be a non-negative integer.'
 
-    return sign*sum
+  cache_dictionary = {0: np.identity(np.shape(X)[0]), 1: X}
 
-def _matrix_approximate_cosine_function(X, n=10):
-    
-    assert np.shape(X)[0] == np.shape(X)[1], 'Matrix argument must be a square matrix.'
-    # assert (X <= np.pi/2.).all() and (X >= np.pi/2.).all(), 'The absolute value of all elements of the matrix must be lower than π/2.'
-    assert (n >= 0) and isinstance(n, int), 'n must be a positive integer greater than 1.'
+  if n in cache_dictionary:
+    return cache_dictionary[n]
+  
+  cache_dictionary[n] = 2*np.matmul(X, scalar_Chebyshev_polynomial_term_recursive_function(X, n-1)) - scalar_Chebyshev_polynomial_term_recursive_function(X, n-2)
 
-    matrix_dimension = np.shape(X)[0]
-    squared_matrix = matrix_power(X, 2)
-    sum = np.zeros(np.shape(X))
-    # , dtype=np.complex_)
-    for k in range(n+1):
-        sum += ((-1)**(k)/factorial(2*k))*matrix_power(squared_matrix, k)
-    
-    return sum
+  return cache_dictionary[n]
 
-def _matrix_approximate_sine_function(X, n=10):
-    
-    assert np.shape(X)[0] == np.shape(X)[1], 'Matrix argument must be a square matrix.'
-    assert (n >= 0) and isinstance(n, int), 'n must be a positive integer greater than 1.'
-
-    matrix_dimension = np.shape(X)[0]
-    sum = np.zeros((matrix_dimension, matrix_dimension))
-    for k in range(n+1):
-        sum += ((-1)**(k)/factorial(2*k+1))*matrix_power(X, 2*k+1)
-    
-    return sum
-
-# def Chebyshev_polynomials_column_function(n, x, b):
-
-#   Tn_minus1 = b
-
-#   if (n == 0):
-#     return Tn_minus1
-
-#   else:
-#     Tn = x
-#     for order in range(1, n):
-#       temp = Tn
-#       Tn = 2*(x*Tn) - Tn_minus1
-#       Tn_minus1 = temp
-
-#     return Tn
-
-# def approximate_matrix_sign_function(array, N=50):
-
-# 	eg_val, eg_vect = linalg.eig(array)
-
-# 	alpha = np.min(np.absolute(eg_val))
-# 	beta = np.max(np.absolute(eg_val))
-
-# 	X = (1/(beta**2 - alpha**2))*( 2*linalg.fractional_matrix_power(array, 2) - (beta**2 + alpha**2)*np.identity(np.shape(array)[0], dtype=np.complex_) )
-
-# 	temp_sum = np.zeros( np.shape(array), dtype=np.complex_)
-# 	for n in range(N):
-# 		temp_sum += Chebyshev_polynomials_factors(n, N, alpha, beta)*matrix_Chebyshev_polynomials_function(X, n)
-
-# 	return np.matmul(array, temp_sum)
-
-# # GAUGED OPERATORS
-
+###########################################################
 def Chebyshev_polynomials_factors(n, N, alpha, beta):
 
   sum = 0.0
@@ -151,7 +178,7 @@ def Chebyshev_polynomials_factors(n, N, alpha, beta):
   if (n==0):
     factor = 1.0/float(N)
   else:
-    factor = 2.0/float(N)
+    factor = 20/float(N)
   
   return factor*sum
 
@@ -162,7 +189,7 @@ def recursive_Chebyshev_polynomials_matrix_function(X, n):
 	elif (n == 1):
 		return X
 	else:
-		return 2.0*np.matmul(X, recursive_Chebyshev_polynomials_matrix_function(X, n-1)) - recursive_Chebyshev_polynomials_matrix_function(X, n-2)
+		return 20*np.matmul(X, recursive_Chebyshev_polynomials_matrix_function(X, n-1)) - recursive_Chebyshev_polynomials_matrix_function(X, n-2)
 
 
 def Chebyshev_polynomials_matrix_function(X, n):
@@ -179,6 +206,7 @@ def Chebyshev_polynomials_matrix_function(X, n):
       Tn_minus1 = temp
 
     return Tn
+
 
 def Chebyshev_polynomials_column_function(n, x, b):
 
@@ -198,24 +226,21 @@ def Chebyshev_polynomials_column_function(n, x, b):
 
 
 def approximate_matrix_sign_function(array, N=50):
+  '''Input: '''
 
-	# eg_val, eg_vect = linalg.eig( np.matmul(np.conjugate(array.T), array) )
+  eg_val, _ = linalg.eig( np.matmul(np.conjugate(array.T), array) )
 
-	# alpha = np.sqrt(np.min(np.absolute(eg_val)))
-	# beta = np.sqrt(np.max(np.absolute(eg_val)))
+  alpha = np.sqrt(np.min(np.absolute(eg_val)))
+  beta = np.sqrt(np.max(np.absolute(eg_val)))
 
-	eg_val, eg_vect = linalg.eig(array)
+  X = (1/(beta**2 - alpha**2))*( 2*np.matmul(np.conjugate(array.T), array) - (beta**2 + alpha**2)*np.identity(np.shape(array)[0], dtype=np.complex_) )
 
-	alpha = np.min(np.absolute(eg_val))
-	beta = np.max(np.absolute(eg_val))
+  temp_sum = np.zeros( np.shape(array), dtype=np.complex_)
+  for n in range(N):
+    temp_sum += Chebyshev_polynomials_factors(n, N, alpha, beta)*Chebyshev_polynomials_matrix_function(X, n)
 
-	X = (1/(beta**2 - alpha**2))*( 2*linalg.fractional_matrix_power(array, 2) - (beta**2 + alpha**2)*np.identity(np.shape(array)[0], dtype=np.complex_) )
+  return np.matmul(array, temp_sum)
 
-	temp_sum = np.zeros( np.shape(array), dtype=np.complex_)
-	for n in range(N):
-		temp_sum += Chebyshev_polynomials_factors(n, N, alpha, beta)*Chebyshev_polynomials_matrix_function(X, n)
-
-	return np.matmul(array, temp_sum)
 
 def formal_sign_function(array):
 
@@ -228,6 +253,7 @@ def formal_sign_function(array):
 
 def r(x, alpha, beta):
   return (0.5*(beta**2 + alpha**2) + 0.5*x*(beta**2 - alpha**2))**(-0.5)
+
 
 def Chebyshev_polynomials_function(x, n):
 
@@ -243,6 +269,7 @@ def Chebyshev_polynomials_function(x, n):
 
     return Tn
 
+
 def Chebyshev_polynomials_factors(n, N, alpha, beta):
 
   sum = 0.0
@@ -254,96 +281,19 @@ def Chebyshev_polynomials_factors(n, N, alpha, beta):
   if (n==0):
     factor = 1.0/float(N)
   else:
-    factor = 2.0/float(N)
+    factor = 20/float(N)
   
   return factor*sum
 
-#   # return np.pi/float(N)*sum
-#   if (n == 0):
-#     factor = 1.0/float(N)
-#   else:
-#     factor = 2.0/float(N)
-  
-#   return factor*sum
 
-# def r(x, alpha, beta):
-#   return (0.5*(beta**2 + alpha**2) + 0.5*x*(beta**2 - alpha**2))**(-0.5)
+def matrix_sign_function_array_function(input_matrix):
+  '''Implementation of the formal definition of the sign function for a matrix argument.'''
 
-# class OverlapOperator:
-#     def __init__(self, lattice_size, lattice_dimensions, fermion_dimensions, gauge_links_phase_values_field_array, laplacian_stencil, derivative_stencil, bare_mass, alpha, beta):
-#         self.gauge_links_phase_values_field_array = gauge_links_phase_values_field_array
-#         self.laplacian_stencil = laplacian_stencil
-#         self.derivative_stencil = derivative_stencil
-#         self.bare_mass = bare_mass
-#         self.alpha = alpha
-#         self.beta = beta
-#         self.lattice_size = lattice_size
-#         self.lattice_dimensions = lattice_dimensions
-#         self.fermion_dimensions = fermion_dimensions
-    
-#     def reduced_Wilson_operator_function(self, x):
-#         return improved_Wilson_operator_linear_function(self.gauge_links_phase_values_field_array, self.laplacian_stencil, self.derivative_stencil, x)
+  input_matrix = np.array(input_matrix)
+  assert (input_matrix.ndim == 2) and (input_matrix.shape[0] == input_matrix.shape[1]), 'Input matrix must be a 2-dimensional square array.'
 
-#     def h_vector(self, x):
-#         return gamma5_function(self.reduced_Wilson_operator_function(x)) - gamma5_function(x)
+  input_matrix_norm_squared = np.matmul(input_matrix, np.conjugate(input_matrix.T))
+  inverse_of_input_matrix_norm_squared = fractional_matrix_power(input_matrix_norm_squared, -0.5)
+  sign_function_of_input_matrix = np.matmul(input_matrix, inverse_of_input_matrix_norm_squared)
 
-#     def X_column(self, x):
-#         return (1/(self.beta**2 - self.alpha**2))*( 2*self.h_vector(self.h_vector(x)) - (self.beta**2 + self.alpha**2)*x )
-
-#     def Chebyshev_polynomials_vector_function(self, n, x):
-#         Tn_minus1 = x
-#         if (n == 0):
-#             return Tn_minus1
-#         else:
-#             Tn = self.X_column(x)
-#             for order in range(1, n):
-#                 temp = Tn
-#                 Tn = 2*self.X_column(Tn) - Tn_minus1
-#                 Tn_minus1 = temp
-
-#         return Tn
-    
-#     def approximate_matrix_sign_column_function(self, x, N=50):
-#         vector_sum = np.zeros( (np.shape(x)[0]), dtype=np.complex_)
-#         for n in range(N):
-#             vector_sum += Chebyshev_polynomials_factors(n, N, self.alpha, self.beta)*self.Chebyshev_polynomials_vector_function(n, x)
-
-#         result = self.h_vector(vector_sum)
-
-#         return result
-    
-#     def overlap_operator_linear_function(self, x):
-#         return x + gamma5_function(self.approximate_matrix_sign_column_function(self.gauge_links_phase_values_field_array, self.laplacian_stencil, self.derivative_stencil, self.alpha, self.beta, x))
-
-#     def massive_overlap_operator_linear_function(self, x):
-#         return (1-0.5*self.bare_mass)*self.overlap_operator_linear_function(self.gauge_links_phase_values_field_array, self.laplacian_stencil, self.derivative_stencil, x) + self.bare_mass*x
-
-#     def inverse_overlap_operator_function(self, number_of_rows=4):
-
-#         x0 = np.zeros((self.fermion_dimensions*(self.lattice_size**self.lattice_dimensions),), dtype=np.complex_)
-#         psi_vector_field = np.zeros((self.fermion_dimensions*(self.lattice_size**self.lattice_dimensions),), dtype=np.complex_)
-#         psi_vector_field[0] = 1.0
-
-#         inverse_overlap_operator = []
-#         for next_permutation in range(number_of_rows):
-            
-#             b_prime = gamma5_function(self.overlap_operator_linear_function(self.gauge_links_phase_values_field_array, self.laplacian_stencil, self.derivative_stencil, gamma5_function(psi_vector_field)))
-            
-#             inverse_overlap_operator_vector = functional_conjugate_gradient_solution_function(self.gauge_links_phase_values_field_array, self.laplacian_stencil, self.derivative_stencil, b_prime, x0)
-            
-#             inverse_overlap_operator.append(inverse_overlap_operator_vector)
-
-#             psi_vector_field = np.roll(psi_vector_field, shift=1)
-
-#         inverse_overlap_operator = (np.array(inverse_overlap_operator))
-
-#         return inverse_overlap_operator.T
-
-# def recursive_Chebyshev_polynomials_function(x, n):
-
-#   if (n == 0):
-#     return 1
-#   elif (n == 1):
-#     return x
-#   else:
-#   	return 2*x*recursive_Chebyshev_polynomials_function(x, n-1) - recursive_Chebyshev_polynomials_function(x, n-2)
+  return sign_function_of_input_matrix
