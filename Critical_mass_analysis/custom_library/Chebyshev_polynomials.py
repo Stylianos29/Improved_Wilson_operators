8fe4ac1a3a2ff09eed4579ebca4636c0ded663cf
Stylianos Gregoriou <s.gregoriou@cyi.ac.cy>
2023-10-08 16:03:32 +0300

    Added class members

--- Critical_mass_analysis/custom_library/Chebyshev_polynomials.py
+++ Critical_mass_analysis/custom_library/Chebyshev_polynomials.py
@@ -1,8 +1,6 @@
-'''
-Scalar and matrix functions for constructing Chebyshev polynomial terms of the first kind and an approximate matrix sign .
-TODO: Check if functions work for complex arguments.
-'''
+'''This module contains the ChebyshevPolynomialsSignFunction class definition along with a number of functions solely included for testing purposes. Scalar and matrix functions for constructing Chebyshev polynomial terms of the first kind and an approximate matrix sign.'''
 
+from typing import Any
 import numpy as np
 from numpy.linalg import matrix_power
 from math import factorial
@@ -10,7 +8,9 @@ from scipy.linalg import fractional_matrix_power
 from scipy import linalg
 
 
-# SCALAR FUNCTIONS
+#############################
+#     SCALAR FUNCTIONS
+#############################
 
 # SCALAR TRIGONOMETRIC FUNCTIONS
 
@@ -94,12 +94,115 @@ def scalar_Chebyshev_polynomial_term_recursive_function(x, n):
 
   return cache_dictionary[n]
 
+class ScalarChebyshevPolynomialsSignFunction:
 
-# MATRIX FUNCTIONS
+  def __init__(self, alpha, beta):
+
+    assert isinstance(alpha, float) and (alpha > 0), ''
+    self.alpha = alpha
+
+    assert isinstance(beta, float) and (beta > 0), ''
+    self.beta = beta
+
+  def __call__(self, x, N):
+  
+    assert isinstance(x, float) and (x >= -1.) and (x <= 1.), 'Input argument must be a real number in the range [-1., 1.].'
+
+    assert isinstance(N, int) and (N >= 0), 'The total number of terms parameter N must be a non-negative integer.'
+    self.N = N
+
+    Chebyshev_polynomials_expansion_sum = 0.
+    for n in range(self.N):
+      Chebyshev_polynomials_expansion_sum += self.Chebyshev_polynomials_summation_factors(n)*scalar_Chebyshev_polynomial_term_function(x, n)
+
+    return Chebyshev_polynomials_expansion_sum 
+
+  def r(self, x):
+    return (0.5*(self.beta**2 + self.alpha**2) + 0.5*x*(self.beta**2 - self.alpha**2))**(-0.5)
+  
+  def xk(self, k):
+    return np.cos(np.pi/self.N*(k+0.5))
+
+  def Chebyshev_polynomials_summation_factors(self, n):
+
+    sum = 0.0
+    for k in range(self.N):
+      sum += self.r(self.xk(k))*scalar_Chebyshev_polynomial_term_function(self.xk(k), n)
+
+    pre_factor = 2
+    if (n == 0):
+      pre_factor = 1
+    
+    return pre_factor/float(self.N)*sum
+
+
+class new_ScalarChebyshevPolynomialsSignFunction:
+
+  def __init__(self, alpha, beta):
+
+    assert isinstance(alpha, float) and (alpha > 0), ''
+    self.alpha = alpha
+
+    assert isinstance(beta, float) and (beta > 0), ''
+    self.beta = beta
+
+  def __call__(self, x, N):
+  
+    assert isinstance(x, float) and (x >= -1.) and (x <= 1.), 'Input argument must be a real number in the range [-1., 1.].'
+
+    assert isinstance(N, int) and (N >= 0), 'The total number of terms parameter N must be a non-negative integer.'
+    self.N = N
+
+    # Initializing cache dictionaries
+    self.discrete_xk_values_dict = dict()
+    self.discrete_rk_values_dict = dict()
+
+    Chebyshev_polynomials_expansion_sum = 0.
+    for n in range(self.N):
+      Chebyshev_polynomials_expansion_sum += self.Chebyshev_polynomials_summation_factors(n)*scalar_Chebyshev_polynomial_term_function(x, n)
+
+    return Chebyshev_polynomials_expansion_sum 
+
+  def r(self, x):
+    return (0.5*(self.beta**2 + self.alpha**2) + 0.5*x*(self.beta**2 - self.alpha**2))**(-0.5)
+  
+  def xk(self, k):
+
+    if k in self.discrete_xk_values_dict:
+      return self.discrete_xk_values_dict[k]
+
+    self.discrete_xk_values_dict[k] = np.cos(np.pi/self.N*(k+0.5))
+
+    return self.discrete_xk_values_dict[k]
+
+  def Chebyshev_polynomials_summation_factors(self, n):
+
+    # helper function
+    def rk(k):
+      if k in self.discrete_rk_values_dict:
+        return self.discrete_rk_values_dict[k]
+      
+      self.discrete_rk_values_dict[k] = self.r(self.xk(k))
+
+      return self.discrete_rk_values_dict[k]
+    
+    expansion_sum = 0.0
+    for k in range(self.N):
+      expansion_sum += rk(k)*scalar_Chebyshev_polynomial_term_function(self.xk(k), n)
+
+    pre_factor = 2
+    if (n == 0):
+      pre_factor = 1
+    
+    return pre_factor/float(self.N)*expansion_sum
+
+#############################
+#     MATRIX FUNCTIONS
+#############################
 
 # MATRIX TRIGONOMETRIC FUNCTIONS
 
-def matrix_approximate_cosine_function(X, n=10):
+def matrix_approximate_cosine_function(X, n=20):
   '''Implementation of a truncated Taylor series for approximating the matrix cosine function.'''
 
   X = np.array(X)
@@ -115,7 +218,7 @@ def matrix_approximate_cosine_function(X, n=10):
   return sum
 
 
-def matrix_approximate_cosine_function(X, n=10):
+def matrix_approximate_sine_function(X, n=20):
   '''Implementation of a truncated Taylor series for approximating the matrix cosine function.'''
 
   X = np.array(X)
@@ -142,9 +245,9 @@ def matrix_Chebyshev_polynomial_term_function(X, n):
   if (n == 0):
     return np.identity(np.shape(X)[0])
 
-  Tn_minus1, Tn = np.identity(np.shape(X)[0]), X
+  Tn_minus1, Tn = np.identity(X.shape[0]), X
   for _ in range(1, n):
-    Tn_minus1, Tn = Tn, 2*np.matmul(X, Tn) - Tn_minus1
+    Tn_minus1, Tn = Tn, 2*np.matmul(X,Tn) - Tn_minus1
   
   return Tn
 
@@ -162,53 +265,116 @@ def matrix_Chebyshev_polynomial_term_recursive_function(X, n):
   if n in cache_dictionary:
     return cache_dictionary[n]
   
-  cache_dictionary[n] = 2*np.matmul(X, scalar_Chebyshev_polynomial_term_recursive_function(X, n-1)) - scalar_Chebyshev_polynomial_term_recursive_function(X, n-2)
+  cache_dictionary[n] = 2*np.matmul(X, matrix_Chebyshev_polynomial_term_recursive_function(X, n-1)) - matrix_Chebyshev_polynomial_term_recursive_function(X, n-2)
 
   return cache_dictionary[n]
 
-###########################################################
+# SIGN FUNCTIONS
+
+def matrix_formal_sign_function_function(input_matrix):
+  '''Implementation of the formal definition of the matrix sign function.'''
+
+  input_matrix = np.array(input_matrix)
+  assert (input_matrix.ndim == 2) and (input_matrix.shape[0] == input_matrix.shape[1]), 'Input matrix must be a 2-dimensional square array.'
+
+  input_matrix_norm_squared = np.matmul(input_matrix, np.conjugate(input_matrix.T))
+  inverse_of_input_matrix_norm_squared = fractional_matrix_power(input_matrix_norm_squared, -0.5)
+  sign_function_of_input_matrix = np.matmul(input_matrix, inverse_of_input_matrix_norm_squared)
+
+  return sign_function_of_input_matrix
+
+
+def matrix_approximate_sign_function(array, N=50):
+  '''Input: '''
+
+  eg_val, _ = linalg.eig( np.matmul(np.conjugate(array.T), array) )
+
+  alpha = np.sqrt(np.min(np.absolute(eg_val)))
+  beta = np.sqrt(np.max(np.absolute(eg_val)))
+
+  X = (1/(beta**2 - alpha**2))*( 2*np.matmul(np.conjugate(array.T), array) - (beta**2 + alpha**2)*np.identity(np.shape(array)[0], dtype=np.complex_) )
+
+  temp_sum = np.zeros( np.shape(array), dtype=np.complex_)
+  for n in range(N):
+    temp_sum += Chebyshev_polynomials_factors(n, N, alpha, beta)*matrix_Chebyshev_polynomial_term_function(X, n)
+
+  return np.matmul(array, temp_sum)
+
+#############################
+#     VECTOR FUNCTIONS
+#############################
+
+class MatrixChebyshevPolynomialsSignFunction(ScalarChebyshevPolynomialsSignFunction):
+
+  def __call__(self, H, N=50):
+    
+    H = np.array(H)
+    
+    assert (H.ndim == 2) and (H.shape[0] == H.shape[1]), 'Input matrix must be a 2-dimensional square array.'
+    assert np.all(np.isclose(H, np.transpose(np.conjugate(H)))), 'Input array must be complex Hermitian.'
+    self.H = H
+
+    assert isinstance(N, int) and (N >= 0), 'The number of Chebyshev polynomials terms N must be a non-negative integer.'
+    self.N = N
+
+    self.alpha, self.beta = self. extreme_values_of_the_eigenvalues_spectrum()
+
+    return self.matrix_approximate_sign_function()
+
+  def __str__(self) -> str:
+    return f'An input matrix of shape {self.H.shape} was passed.'
+
+  def Chebyshev_polynomials_factors(self, n):
+
+    inverse_N_factor = 1/float(self.N)
+
+    sum = 0.0
+    for k in range(0, self.N):
+      xk = np.cos((k + 0.5)*np.pi*inverse_N_factor)
+      sum += r(xk, self.alpha, self.beta)*scalar_Chebyshev_polynomial_term_function(xk, n)
+
+    factor = 2
+    if (n == 0):
+      factor = 1
+
+    return factor*inverse_N_factor*sum
+
+  def extreme_values_of_the_eigenvalues_spectrum(self):
+    eg_val, _ = linalg.eig(matrix_power(self.H, 2))
+
+    return np.sqrt(np.min(eg_val)), np.sqrt(np.max(eg_val))
+  
+  def matrix_approximate_sign_function(self):
+
+    X = (1/(self.beta**2 - self.alpha**2))*( 2*matrix_power(self.H, 2) - (self.beta**2 + self.alpha**2)*np.identity((self.H).shape[0], dtype=np.complex_) )
+
+    temp_sum = np.zeros_like(self.H)
+    for n in range(self.N):
+      temp_sum += Chebyshev_polynomials_factors(n, self.N, self.alpha, self.beta)*matrix_Chebyshev_polynomial_term_function(X, n)
+
+    return np.matmul(self.H, temp_sum)
+
+
+##################################
+# Functions kept to avoid dependency conflicts with other scripts, they need to be removed.
+
 def Chebyshev_polynomials_factors(n, N, alpha, beta):
 
   sum = 0.0
   for k in range(1, N+1):
     xk = np.cos((k - 0.5)*np.pi/float(N))
-    sum += r(xk, alpha, beta)*Chebyshev_polynomials_function(xk, n)
+    sum += r(xk, alpha, beta)*scalar_Chebyshev_polynomial_term_function(xk, n)
 
-  # return np.pi/float(N)*sum
   if (n==0):
-    factor = 1.0/float(N)
+    factor = 1/float(N)
   else:
-    factor = 20/float(N)
+    factor = 2/float(N)
   
   return factor*sum
 
 
-def recursive_Chebyshev_polynomials_matrix_function(X, n):
-	if (n == 0):
-		return np.identity(np.shape(X)[0], dtype=np.complex_)
-	elif (n == 1):
-		return X
-	else:
-		return 20*np.matmul(X, recursive_Chebyshev_polynomials_matrix_function(X, n-1)) - recursive_Chebyshev_polynomials_matrix_function(X, n-2)
-
-
-def Chebyshev_polynomials_matrix_function(X, n):
-
-  Tn_minus1 = np.identity(np.shape(X)[0], dtype=np.complex_)
-
-  if (n==0):
-    return Tn_minus1
-  else:
-    Tn = X
-    for order in range(1, n):
-      temp = Tn
-      Tn = 2*np.matmul(X, Tn) - Tn_minus1
-      Tn_minus1 = temp
-
-    return Tn
-
-
 def Chebyshev_polynomials_column_function(n, x, b):
+  '''???????????'''
 
   Tn_minus1 = b
 
@@ -225,75 +391,5 @@ def Chebyshev_polynomials_column_function(n, x, b):
     return Tn
 
 
-def approximate_matrix_sign_function(array, N=50):
-  '''Input: '''
-
-  eg_val, _ = linalg.eig( np.matmul(np.conjugate(array.T), array) )
-
-  alpha = np.sqrt(np.min(np.absolute(eg_val)))
-  beta = np.sqrt(np.max(np.absolute(eg_val)))
-
-  X = (1/(beta**2 - alpha**2))*( 2*np.matmul(np.conjugate(array.T), array) - (beta**2 + alpha**2)*np.identity(np.shape(array)[0], dtype=np.complex_) )
-
-  temp_sum = np.zeros( np.shape(array), dtype=np.complex_)
-  for n in range(N):
-    temp_sum += Chebyshev_polynomials_factors(n, N, alpha, beta)*Chebyshev_polynomials_matrix_function(X, n)
-
-  return np.matmul(array, temp_sum)
-
-
-def formal_sign_function(array):
-
-  array_norm_squared = np.matmul(array, np.conjugate(array.T))
-  inverse_array_norm = fractional_matrix_power(array_norm_squared, -0.5)
-  sign_function_array = np.matmul(array, inverse_array_norm)
-
-  return sign_function_array
-
-
 def r(x, alpha, beta):
   return (0.5*(beta**2 + alpha**2) + 0.5*x*(beta**2 - alpha**2))**(-0.5)
-
-
-def Chebyshev_polynomials_function(x, n):
-
-  Tn_minus1 = 1
-  if (n==0):
-    return Tn_minus1
-  else:
-    Tn = x
-    for order in range(1, n):
-      temp = Tn
-      Tn = 2*x*Tn - Tn_minus1
-      Tn_minus1 = temp
-
-    return Tn
-
-
-def Chebyshev_polynomials_factors(n, N, alpha, beta):
-
-  sum = 0.0
-  for k in range(1, N+1):
-    xk = np.cos((k - 0.5)*np.pi/float(N))
-    sum += r(xk, alpha, beta)*Chebyshev_polynomials_function(xk, n)
-
-  # return np.pi/float(N)*sum
-  if (n==0):
-    factor = 1.0/float(N)
-  else:
-    factor = 20/float(N)
-  
-  return factor*sum
-
-
-def matrix_sign_function_array_function(input_matrix):
-  '''Implementation of the formal definition of the sign function for a matrix argument.'''
-
-  input_matrix = np.array(input_matrix)
-  assert (input_matrix.ndim == 2) and (input_matrix.shape[0] == input_matrix.shape[1]), 'Input matrix must be a 2-dimensional square array.'
-
-  input_matrix_norm_squared = np.matmul(input_matrix, np.conjugate(input_matrix.T))
-  inverse_of_input_matrix_norm_squared = fractional_matrix_power(input_matrix_norm_squared, -0.5)
-  sign_function_of_input_matrix = np.matmul(input_matrix, inverse_of_input_matrix_norm_squared)
-
-  return sign_function_of_input_matrix
